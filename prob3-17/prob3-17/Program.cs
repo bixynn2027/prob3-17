2592521d5e05506f810c4d351d6d8f139c5f51f0
bixynn2027 <59707557+bixynn2027@users.noreply.github.com>
2020-01-27 01:53:46 +0900

    例題の作成

---
+++ prob3-17/prob3-17/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prob3_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数を入力してください(1-100)：");
+            int a = int.Parse(Console.ReadLine());
+            if (a <= 0 || a > 100)
+            {
+                Console.WriteLine("適切な値を入力してください");
+            }
+            if (a < 50 && a % 2 == 0)
+            {
+                Console.WriteLine("50以下の偶数です");
+            }
+            else if (a < 50)
+            {
+                Console.WriteLine("50以下です");
+            }
+            else if (a % 2 == 0)
+            {
+                Console.WriteLine("偶数です");
+            }
+        }
+    }
+}
